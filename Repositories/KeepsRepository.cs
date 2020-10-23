73ea60d43cb3e2aeedbedd447388df8cf935946e
Lawrence Haney <Lawrence.r.haney@gmail.com>
2020-10-23 13:45:16 -0600

    keep 0.02 skeleton Repo untested

---
+++ Repositories/KeepsRepository.cs
@@ -0,0 +1,62 @@
+using System.Data;
+using System.Linq;
+using Dapper;
+using Keepr.Models;
+
+namespace Keepr.Repositories
+{
+  public class KeepsRepository
+  {
+    //defines DB
+    private readonly IDbConnection _db;
+    //Insures program integrity by ofuscating db
+    public KeepsRepository(IDbConnection db)
+    {
+      _db= db;
+    }
+    //SQL string to populate profile
+    private readonly string populateCreator = @"
+    SELECT
+    keep.*,
+    profile.*
+    FROM keeps keep
+    JOIN profiles profile on keep.creatorEmail = profile.email
+    ";
+    //SQL call to create new table item
+    internal int Create(Keep newKeep)
+    {
+      string sql =@"
+      INSERT INTO Keeps
+      (creatorId, name, description, img)
+      VALUES
+      (@CreatorId, @Name, @Description, @Img);
+      SELECT LAST_INSERT_ID();";
+      return _db.ExecuteScalar<int>(sql, newKeep);
+    }
+    //SQL Call to GetAll keeps and populate profile info from populateCreator
+    internal IEnumerable<Keep> GetAll()
+    {
+      string sql = populateCreator;
+      return _db.Query<Keep, Profile, Keep>(
+        sql, (keep, profile)=> 
+        {keep.Creator = profile; return keep;}, 
+        splitOn: "id");
+    }
+
+    //SQL call that returns a keep by id and populates the creator profile
+    internal Keep GetById(int id)
+    {
+      string sql = populateCreator + "WHERE keep.id = @id";
+      return _db.Query<Keep, Profile, Keep>(
+        sql, (keep, profile)=> 
+        {keep.Creator = profile; return keep;}, 
+        splitOn: "id").FirstOrDefault();
+    }
+
+    internal void DeleteKeep(int id)
+    {
+      string sql = "DELETE FROM Keeps WHERE id = @id"
+      _db.Execute(sql, new {id});
+    }
+  }
+}
