89af96089762f10835fe15f054b54e54ebe5d029
Lawrence Haney <Lawrence.r.haney@gmail.com>
2020-10-27 10:16:44 -0600

    Vkeeps .006 add api routes for create and delete

---
+++ Controllers/VaultKeepsController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Threading.Tasks;
+using CodeWorks.Auth0Provider;
+using Keepr.Models;
+using Keepr.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keepr.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+
+  public class VaultKeepsController : ControllerBase
+  {
+    private readonly VaultKeepsService _serv;
+
+    public VaultKeepsController(VaultKeepsService serv)
+    {
+      _serv = serv;
+    }
+
+    [HttpPost]
+    [Authorize]
+
+    public async Task<ActionResult<VaultKeep>> Create([FromBody] VaultKeep newVaultKeep)
+    {
+      try
+      {
+        Profile userinfo = await HttpContext.GetUserInfoAsync<Profile>();
+        newVaultKeep.CreatorId = userinfo.Id;
+        return Ok(_serv.Create(newVaultKeep));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpDelete("{id}")]
+    [Authorize]
+
+    public async Task<ActionResult<VaultKeep>> Delete(int id)
+    {
+      try
+      {
+        Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
+        return Ok(_serv.Delete(id, userInfo.Id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+  }
+}
